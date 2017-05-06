cdccf06558e277068ffdd4a61174fc3296da8954
Prashant <prashant.github@gmail.com>
2017-05-06 09:36:14 +0530

    Add files via upload

---
+++ javascript/js/main.js
@@ -0,0 +1,20 @@
+var myheading=document.querySelector("h1");
+myheading.textContent = "hello world";
+
+var icecream = "mango";
+if(icecream ==="strawarbry"){
+    alert("i love mango");
+}
+else{
+    alert("i love choclate al lot then mango");
+    var newheading = document.querySelector("h2");
+    newheading.textContent = "i love choclate al lot then mango";
+}
+
+function multiply(num1, num2){
+  var myresult = num1 + num2;
+
+return myresult;
+}
+
+var image = 
